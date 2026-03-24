61409398ea25122ca02f2613d7e2944ceda3ade3
KUMU-THA <kumutha182004@gmail.com>
2026-03-24 20:41:49 +0530

    prepare backend for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const app = require("./app");
 
 require("./db/db");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
